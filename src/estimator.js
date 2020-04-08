77272b23a51d489fe025320f410e78e0910432e2
marusoft <alimi.kmaruf@gmail.com>
2020-04-08 15:25:58 +0100

    complete challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const { period, timeToProceed, reportedCases } = data;
+  const currentlyInfectedNonExpert = reportedCases * 10;
+  // HMS/MITGH report cases
+  const currentlyInfectedByExpert = reportedCases * 50;
+  let estimateMultiplier;
+  if (period === 'weeks') {
+    estimateMultiplier = timeToProceed * 7;
+  } else if (period === 'months') {
+    estimateMultiplier = timeToProceed * 30;
+  } else {
+    estimateMultiplier = timeToProceed;
+  }
+  return {
+    data,
+    impact: {
+      currentlyInfected: currentlyInfectedNonExpert,
+      infectionsByRequestedTime: currentlyInfectedNonExpert * 2 ** (estimateMultiplier / 3)
+    },
+    severeImpact: {
+      currentlyInfected: currentlyInfectedByExpert,
+      infectionsByRequestedTime: currentlyInfectedByExpert * 2 ** (estimateMultiplier / 3)
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
