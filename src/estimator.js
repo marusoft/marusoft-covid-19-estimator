3850d3ea4624fed2b73b61de155477217bd51795
marusoft <alimi.kmaruf@gmail.com>
2020-04-13 06:37:04 +0100

    complete challenge 1, 2 and 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,27 +1,75 @@
-/* eslint-disable max-len */
 const covid19ImpactEstimator = (data) => {
-  let estimatedFactor;
-  const time = data.timeToElapse;
-  const impact = {};
-  const severeImpact = {};
-  if ((data.periodType) === 'days') {
-    estimatedFactor = Math.floor((time / 3));
-  } else if ((data.periodType) === 'weeks') {
-    estimatedFactor = Math.floor(((7 * time) / 3));
-  } else if ((data.periodType) === 'months') {
-    estimatedFactor = Math.floor(((30 * time) / 3));
+  const {
+    reportedCases,
+    timeToElapse,
+    periodType,
+    totalHospitalBeds,
+    region
+  } = data;
+  let factor;
+  let period;
+
+  if (periodType === 'days') {
+    factor = Math.floor(timeToElapse / 3);
+    period = timeToElapse;
+  } else if (periodType === 'weeks') {
+    factor = Math.floor((timeToElapse * 7) / 3);
+    period = timeToElapse * 7;
+  } else {
+    factor = Math.floor((timeToElapse * 30) / 3);
+    period = timeToElapse * 30;
   }
-  impact.currentlyInfected = ((data.reportedCases) * 10);
-  severeImpact.currentlyInfected = ((data.reportedCases) * 50);
-  impact.infectionsByRequestedTime = (impact.currentlyInfected * (2 ** estimatedFactor));
-  severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * (2 ** estimatedFactor));
-  const result = {
+  // impact estimations
+  const impactCurrentlyInfected = reportedCases * 10;
+  const impactInfectionsByRequestedTime = impactCurrentlyInfected * (2 ** factor);
+  const casesByRequestedTime = Math.ceil(impactInfectionsByRequestedTime * 0.15);
+  const totalBeds = Math.ceil(totalHospitalBeds * 0.35);
+  const impactHospitalBedsByRequestedTime = Math.ceil(totalBeds - casesByRequestedTime);
+  const impactCasesForICUByRequestedTime = Math.floor(impactInfectionsByRequestedTime * 0.05);
+  const impactCasesForVentilatorsByRequestedTime = Math.floor(impactInfectionsByRequestedTime
+                                                  * 0.02);
+  const impactDollarsInFlight = Math.floor((impactInfectionsByRequestedTime
+                                    * region.avgDailyIncomePopulation
+                                    * region.avgDailyIncomeInUSD) / period);
+
+  // severe impact estimations
+  const severeImpactCurrentlyInfected = reportedCases * 50;
+  const severeImpactInfectionsByRequestedTime = severeImpactCurrentlyInfected * (2 ** factor);
+  const severeCasesByRequestedTime = Math.ceil(severeImpactInfectionsByRequestedTime * 0.15);
+  const hospitalBedsByRequestedTime = Math.ceil(totalBeds - severeCasesByRequestedTime);
+  const casesForICUByRequestedTime = Math.floor(severeImpactInfectionsByRequestedTime * 0.05);
+  const casesForVentilatorsByRequestedTime = Math.floor(severeImpactInfectionsByRequestedTime
+                                              * 0.02);
+  const dollarsInFlight = Math.floor((severeImpactInfectionsByRequestedTime
+                            * region.avgDailyIncomePopulation
+                            * region.avgDailyIncomeInUSD) / period);
+
+
+  const impact = {
+    currentlyInfected: impactCurrentlyInfected,
+    infectionsByRequestedTime: impactInfectionsByRequestedTime,
+    severeCasesByRequestedTime: casesByRequestedTime,
+    hospitalBedsByRequestedTime: impactHospitalBedsByRequestedTime,
+    casesForICUByRequestedTime: impactCasesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime: impactCasesForVentilatorsByRequestedTime,
+    dollarsInFlight: impactDollarsInFlight
+  };
+
+  const severeImpact = {
+    currentlyInfected: severeImpactCurrentlyInfected,
+    infectionsByRequestedTime: severeImpactInfectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+
+  return {
     data,
     impact,
     severeImpact
   };
-  console.log(result);
-  return result;
 };
 
 export default covid19ImpactEstimator;
