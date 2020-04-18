a2ee674307a6b367c2b1a5e4a316be3956a30f0d
marusoft <alimi.kmaruf@gmail.com>
2020-04-18 11:57:28 +0100

    create logs endpoint

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
 import helmet from 'helmet';
 import jsonToXml from 'jsontoxml';
-// import fs from 'fs';
+import logger from 'morgan';
+import fs from 'fs';
 import estimatorData from './estimator';
 
 
 const app = express();
+
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.txt'), { flags: 'a' });
+logger.token('response', (req, res) => {
+  if (!res._header || !req._startAt) return '';
+  const diff = process.hrtime(req._startAt);
+  let ms = diff[0] * 1e3 + diff[1] * 1e-6;
+  ms = ms.toFixed(0);
+  return `${ms.toString().padStart(2, '0')}ms`;
+});
+
+app.use(
+  logger(':method :url :status :response\n', {
+    stream: accessLogStream
+  })
+);
 
 app.use(helmet());
 app.use(cors());
@@ -41,14 +60,14 @@ app.post('/api/v1/on-covid-19/xml', async (req, res) => {
   res.send(jsonToXml(use));
 });
 
-// app.get('/api/v1/on-covid-19/logs?', async (req, res) {
-//   fs.readFile('.logger.log', 'utf8', (err, data) => {
-//     if(err) throw err;
-//     res.set('Conttent-Type', 'text/plain')
-//     res.send();
-//   })
-// })
+app.get('/api/v1/on-covid-19/logs', (req, res) => {
+  fs.readFile('./src/access.txt', (err, data) => {
+    if (err) throw err;
+    res.header('Content-Type', 'text/plain');
+    res.send(data);
+  });
+});
 
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 2424;
 module.exports = app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
